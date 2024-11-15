c66086316f08f6c1dcdf2a9c38eb986008e72b0a
Snehendu <119668299+datta-boop@users.noreply.github.com>
2024-11-16 00:33:40 +0530

    datta-boop-patch-2

--- charts.py
+++ charts.py
@@ -1,30 +1,181 @@
 import altair as alt
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 def plot_ev_makers_by_state(df):
-    state_ev_maker = df.groupby('State')['EV Maker'].count().to_frame('Total Manufacturing Plants').sort_values(by='Total Manufacturing Plants', ascending=True).reset_index()
-    chart = alt.Chart(state_ev_maker).mark_bar(color='#880808').encode(
+    state_ev_maker = df.groupby('State')['EV Maker'].count().to_frame('Total Manufacturing Plants').sort_values(by='Total Manufacturing Plants', ascending=False).reset_index()
+    chart = alt.Chart(state_ev_maker).mark_bar().encode(
         x=alt.X('State', sort=None),
-        y='Total Manufacturing Plants'
+        y='Total Manufacturing Plants',
+        color = alt.Color('Total Manufacturing Plants:Q',
+                          scale=alt.Scale(scheme='reds'),
+                                          legend=None)
     ).properties(
-        title='Total Manufacturing Plants by State',
+        title='Most Manufacturing Plants per State',
         padding={'left': 10, 'right': 10, 'top': 10, 'bottom': 10}
     )
     st.altair_chart(chart, use_container_width=True)
 
 def plot_ev_makers_by_city(df):
-    city_ev_maker = df.groupby('Place')['EV Maker'].count().to_frame('Total Manufacturing Plants').sort_values(by='Total Manufacturing Plants', ascending=True).reset_index()
-    chart = alt.Chart(city_ev_maker).mark_bar(color='#880808').encode(
+    city_ev_maker = df.groupby('Place')['EV Maker'].count().to_frame('Total Manufacturing Plants').sort_values(by='Total Manufacturing Plants', ascending=False).reset_index()
+    chart = alt.Chart(city_ev_maker.head(10)).mark_bar().encode(
         x=alt.X('Place', sort=None),
-        y='Total Manufacturing Plants'
+        y='Total Manufacturing Plants',
+        color = alt.Color('Total Manufacturing Plants:Q',
+                          scale=alt.Scale(scheme='reds'),
+                                          legend=None)
     ).properties(
-        title='Total Manufacturing Plants by City',
+        title='Most Manufacturing Plants per City (Top 10)',
         padding={'left': 10, 'right': 10, 'top': 10, 'bottom': 10}
     )
     st.altair_chart(chart, use_container_width=True)
 
 def plot_ev_sales_growth(df_ev_sales):
+    # Prepare the data
     df_ev_growth = df_ev_sales[['2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024']].sum().reset_index()
     df_ev_growth.columns = ['Year', 'Total EV Sales']
-    st.line_chart(df_ev_growth.set_index('Year'), x_label='Years', y_label='Total EV Sales', color=['#880808'])
+
+    # Add a subheader in Streamlit
+    st.subheader('All EV Sales YOY Growth')
+
+    # Create the line chart
+    fig = px.line(
+        df_ev_growth,
+        x='Year',
+        y='Total EV Sales',
+        labels={'Year': 'Years', 'Total EV Sales': 'Total EV Sales'}
+    )
+    # Customize the line's appearance
+    fig.update_traces(
+        line_color='#880808',  # Line color
+        line_width=2,          # Set line width
+        mode='lines+markers',  # Add markers at each data point
+        marker=dict(size=8)    # Set marker size
+    )
+
+    # Display the Plotly chart in Streamlit
+    st.plotly_chart(fig, use_container_width=True)
+
+
+def plot_yearly_sales_category(filtered_data, year_cols):
+    yearly_sales_data = filtered_data.groupby('Cat')[year_cols].sum().reset_index()
+    fig_yearly_sales = px.bar(yearly_sales_data.melt(id_vars='Cat', var_name='Year', value_name='Sales'),
+                              x='Year', y='Sales', color='Cat')
+    st.subheader("Yearly Sales by Category")
+    st.plotly_chart(fig_yearly_sales)
+
+def plot_top_makers(filtered_data, year_cols, year):
+    # Prepare data for top manufacturers
+    top_makers_data = filtered_data.groupby('Maker')[year_cols].sum().reset_index()
+    top_makers_data['Total'] = top_makers_data[year_cols].sum(axis=1)
+    top_makers_data = top_makers_data.sort_values('Total', ascending=False).head(10)
+    
+    # Capture the name of the top manufacturer
+    top_maker_name = top_makers_data.iloc[0]['Maker']
+    
+    # Plot the bar chart with a gradient color
+    fig_top_makers = px.bar(
+        top_makers_data,
+        x='Total',
+        y='Maker',
+        orientation='h',
+        title=f"Top 10 Manufacturers in {year}",
+        labels={'Total': 'Total Sales', 'Maker': 'Manufacturer'},
+        color='Total',  # Use the 'Total' column for the gradient
+        color_continuous_scale='Reds'  # Apply a red gradient
+    )
+    
+    # Display the chart in Streamlit
+    st.plotly_chart(fig_top_makers)
+    
+    return top_maker_name  # Return the top manufacturer's name
+
+
+
+def plot_top_market_share(df_ev_sales, selected_maker,year):
+    filtered_data = df_ev_sales[df_ev_sales['Maker'] == selected_maker]
+
+    # Group by category and sum sales
+    sales_by_category = filtered_data.groupby('Cat').sum().drop(columns=['Maker']).reset_index()
+
+    # Bar plot for categories
+    fig = px.bar(
+        sales_by_category,
+        x='Cat',
+        y=year,
+        title=f"Market Share of {selected_maker} in {year}",
+        labels={'Cat': 'Vehicle Category', year: 'Sales'},
+        color='Cat'
+    )
+    st.plotly_chart(fig)
+
+
+def ev_cat_reg(df_ev_reg):
+    chart = alt.Chart(df_ev_reg).mark_bar().encode(
+    x=alt.X('Total Registration:Q', title='Total Registration'),
+    y=alt.Y('Vehicle Class', sort=None, title='Vehicle Class'),
+    color=alt.Color(
+        'Total Registration:Q',
+        scale=alt.Scale(scheme='reds'),
+        legend=None
+        )   
+    ).properties(
+    title='Total Registration of Each Category',
+    padding={'left': 10, 'right': 10, 'top': 10, 'bottom': 10},
+    height=475
+    )
+    st.altair_chart(chart, use_container_width=True)
+
+
+def ev_cat_growth(df_ev_cat_date,selected_cat):
+    filtered_df_cat = df_ev_cat_date.groupby('Year')[selected_cat].sum().to_frame('Total Registration').sort_values(by='Year', ascending = False).reset_index()
+    chart = alt.Chart(filtered_df_cat).mark_bar().encode(
+    x=alt.X('Year:N', title='Year'),
+    y=alt.Y('Total Registration:Q', title='Total Registration'),
+    color=alt.Color('Total Registration:Q', 
+                    scale=alt.Scale(scheme='reds'), 
+                    legend=None)  # Apply red gradient and hide legend
+    ).properties(
+    title=f"YOY Growth of {selected_cat}",
+    width='container',  # Adjusts to container width
+    height=400  # Optional: Set chart height
+    )
+
+    # Display the chart in Streamlit
+    st.altair_chart(chart, use_container_width=True)
+
+
+
+def india_map_plot(df_charging_station,india_states):
+    fig = px.choropleth(df_charging_station, 
+                    locations='id', 
+                    geojson=india_states, 
+                    color='No. of Operational PCS', 
+                    hover_name=df_charging_station['State'],
+                    hover_data=['No. of Operational PCS'])
+
+    fig = px.choropleth_mapbox(df_charging_station, 
+                    locations='id', 
+                    geojson=india_states, 
+                    color='No. of Operational PCS', 
+                    hover_name=df_charging_station['State'],
+                    hover_data={'id': False, 'No. of Operational PCS': True},
+                    mapbox_style='carto-positron',
+                    center={"lat": 20.5937, "lon": 78.9629},
+                    zoom=3.5,
+                    height=800,
+                    color_continuous_scale=[
+
+                        [0.0, "#FFEEEE"],   # Very light red (almost white)
+                        [0.2, "#FF9999"],   # Light red
+                        [0.4, "#FF5555"],   # Medium red
+                        [0.6, "#FF0000"],   # Bright red
+                        [0.8, "#B20000"],   # Dark red
+                        [1.0, "#660000"]    # Very dark red
+                        ]
+                    )
+
+    fig.update_geos(fitbounds='locations', visible=False)
+
+    st.plotly_chart(fig)
--- main.py
+++ main.py
@@ -1,105 +1,187 @@
 import streamlit as st
-import pandas as pd
-import altair as alt
-import plotly.express as px
 import json
 import charts as charts
+import preprocessor
+
+st.set_page_config(page_icon='analysis.ico', page_title='Indian EV Market', layout='wide')
+
+col13,col14 = st.columns([1,4])
+with col13:
+    st.image('Indian EV Market Logo.svg', width=220, use_container_width=False)    
+with col14:
+    st.title('Indian EV Market (2001 - 2024)')
+    st.write("The purpose of the Indian EV Market Dashboard is to provide a comprehensive, interactive visualization of the electric vehicle landscape in India. By analyzing key metrics and trends within the EV market, this dashboard offers insights into EV adoption rates, market share, regional distributions, vehicle types, and growth patterns over time. This tool aims to support data-driven decision-making for stakeholders such as policymakers, industry analysts, and businesses, helping them understand market dynamics and identify emerging opportunities in India’s rapidly evolving EV sector.")
 
 
-st.set_page_config(page_icon='', layout='wide')
 
-st.header('India EV Market (2001 - 2024)')
-st.write("The purpose of the Indian EV Market Dashboard is to provide a comprehensive, interactive visualization of the electric vehicle landscape in India. By analyzing key metrics and trends within the EV market, this dashboard offers insights into EV adoption rates, market share, regional distributions, vehicle types, and growth patterns over time. This tool aims to support data-driven decision-making for stakeholders such as policymakers, industry analysts, and businesses, helping them understand market dynamics and identify emerging opportunities in India’s rapidly evolving EV sector.")
+df = preprocessor.ev_maker()
 
-df = pd.read_csv('EV Maker by Place.csv')
-df_ev_sales = pd.read_csv('ev_sales_by_makers_and_cat_15-24.csv')
+df_ev_sales = preprocessor.ev_sales()
 
-# Sidebar for filters
-st.sidebar.header("Filters")
+df_charging_station = preprocessor.charging_station()
 
-# Filter by vehicle category with "Select All" option
-categories = ["Select All"] + df_ev_sales['Cat'].unique().tolist()
-selected_categories = st.sidebar.multiselect("Select Vehicle Category", categories, default=["Select All"])
+india_states = json.load(open('Indian_States.geojson', 'r'))
 
-# Filter data by selected categories
-if "Select All" in selected_categories:
-    filtered_data = df_ev_sales  # Select all categories
-else:
-    filtered_data = df_ev_sales[df_ev_sales['Cat'].isin(selected_categories)]
+df_ev_cat_date = preprocessor.ev_cat_date()
+df_ev_reg = preprocessor.ev_reg()
 
-# Get unique vehicle makers for the selected categories
-vehicle_makers = filtered_data['Maker'].unique().tolist()
 
-# Multiselect for Vehicle Makers with "Select All" functionality
-vehicle_maker_selection = st.sidebar.multiselect(
-    "Pick Vehicle Maker", 
-    options=["Select All"] + vehicle_makers, 
-    default=["Select All"]
+# KPI Section
+total_sales = df_ev_sales.loc[:, '2015':'2024'].sum().sum()
+top_year = df_ev_sales.loc[:, '2015':'2024'].sum().idxmax()
+top_maker = df_ev_sales.groupby('Maker').sum().loc[:, '2015':'2024'].sum(axis=1).idxmax()
+
+st.subheader("Overall EV Market Analysis")
+col1, col2, col3 = st.columns(3)
+col1.metric("Total EV Units Sold", f"{total_sales}")
+col2.metric("Top Sales Year", top_year)
+col3.metric("Top Manufacturer", top_maker)
+
+col4, col5 = st.columns(2)
+
+# Chart 1: EV Makers by State
+with col4:
+    charts.plot_ev_makers_by_state(df)
+
+# Chart 2: EV Makers by City
+with col5:
+    charts.plot_ev_makers_by_city(df)
+
+col6, col7 = st.columns(2)
+
+# Chart 3: EV Sales Growth
+with col6:
+    charts.plot_ev_sales_growth(df_ev_sales)
+
+# Chart 4: Yearly Sales by Category
+with col7:
+    year_cols = [col for col in df_ev_sales.columns if col.isdigit()]
+    charts.plot_yearly_sales_category(df_ev_sales, year_cols)
+
+
+
+# Filter for specific year selection and display the top 20 chart
+
+st.header('Manufacturer Market Share (Year-on-Year)')
+col8,col9 = st.columns(2)
+
+with col8:
+    year = st.slider(
+    label='Select Year of Sales',
+    min_value=2015,
+    max_value=2024,
+    value=2024,  # Default value when the app loads
+    step=1
+)
+    if year:
+        year = str(year)
+        top_maker_name = charts.plot_top_makers(df_ev_sales, [year],year)  # Capture the top manufacturer name
+with col9:
+    top_makers_data = df_ev_sales.groupby('Maker')[[year]].sum().reset_index()
+    top_makers_data = df_ev_sales.sort_values(by=year, ascending=False).head(10)
+    top_makers_list = top_makers_data['Maker'].tolist()
+
+    selected_maker = st.selectbox(
+"Select a Manufacturer",
+options=top_makers_list,
+index=0
 )
 
-# Implement "Select All" functionality for vehicle makers
-if "Select All" in vehicle_maker_selection:
-    selected_makers = vehicle_makers  # Select all makers
-else:
-    selected_makers = vehicle_maker_selection  # Select specific makers
-
-# Filter data based on selected makers
-filtered_data = filtered_data[filtered_data['Maker'].isin(selected_makers)]
-
-# Optional checkbox to show or hide the filtered data table
-show_data = st.sidebar.checkbox("Show Sales Data for Selected Makers", value=False)
-
-if show_data:
-    st.write(f"### {', '.join(selected_categories)} Sales Data for Selected Makers")
-    st.dataframe(filtered_data)
-
-
-charts.plot_ev_makers_by_state(df)
-
-charts.plot_ev_makers_by_city(df)
+    charts.plot_top_market_share(df_ev_sales, selected_maker,year)  # Use the captured name
 
 
 
-df_ev_growth = df_ev_sales[['2015','2016','2017','2018','2019','2020','2021','2022','2023','2024']]
+top_reg_cat = df_ev_reg.loc[df_ev_reg['Total Registration'].idxmax()]
+top_reg_cat = top_reg_cat['Vehicle Class']
+bot_reg_cat = df_ev_reg.loc[df_ev_reg['Total Registration'].idxmin()]
+bot_reg_cat = bot_reg_cat['Vehicle Class']
 
-charts.plot_ev_sales_growth(df_ev_sales)
+# Display KPIs
+st.header("Growth of EV by Vehicle Class")
+col10, col11 = st.columns(2)
 
-year = st.selectbox(label='Select Year of Sales', options=['2015','2016','2017','2018','2019','2020','2021','2022','2023','2024'], placeholder='Choose an year')
+with col10:
+    st.metric(label="Most Favourite Vehicle Class", value=top_reg_cat)
 
-if year:
-    filtered_df = df_ev_sales[['Maker' , year]]
-    filtered_df = filtered_df.groupby('Maker')[year].sum().to_frame('Total Sales').sort_values(by='Total Sales', ascending=False).reset_index()
-    chart = alt.Chart(filtered_df.head(20)).mark_bar(color='#880808').encode(
-    x=alt.X('Maker', sort=None),  # Keeps the sorted order
-    y='Total Sales'
-    ).properties(
-    title=f'Top 20 EV Manufacturer (Sales) for {year}',
-    padding={'left': 10, 'right': 10, 'top': 10, 'bottom': 10}
-    )
-    st.altair_chart(chart, use_container_width=True)
+with col11:
+    st.metric(label=f"Least Favourite Vehicle Class", value=bot_reg_cat)
+    
 
-df_charging_station = pd.read_csv('OperationalPC.csv')
-with open('Indian_States.json','r') as f:
-    geojson_data = json.load(f)
-# Inspect the GeoJSON data to find the key for state names
-st.write(geojson_data['features'][0]['properties'])
+col20, col21 = st.columns(2)
 
-fig = px.choropleth(
-    df_charging_station,
-    locations='State',  # Column in df representing states
-    locationmode='geojson-id',  # Use 'geojson-id' for locationmode
-    color='No. of Operational PCS',  # Column in df for color scale
-    hover_name='State',  # Display state name on hover
-    geojson=geojson_data,  # GeoJSON data for the map
-    labels={'No. of Operational PCS': 'Operational PCS'},  # Label for the color legend
-    title="Distribution of Charging Stations Across States in India"
-)
+with col20:
+    charts.ev_cat_reg(df_ev_reg)
 
-fig.update_geos(fitbounds="locations", visible=False)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
-st.plotly_chart(fig)
-
-st.dataframe(df_vehicle_class.dtypes)
+with col21:
+    selected_cat = st.selectbox('Select a Vehicle Category', options=df_ev_cat_date.columns[1:-1], index=14)
+    charts.ev_cat_growth(df_ev_cat_date,selected_cat)
 
 
+
+state_id_map = {}
+for feature in india_states['features']:
+    feature['id'] = feature['properties']['state_code']
+    state_id_map[feature['properties']['st_nm']] = feature['id']
+
+df_charging_station['id'] = df_charging_station['State'].apply(lambda x: state_id_map[x])
+
+# Compute KPIs
+total_stations_india = df_charging_station['No. of Operational PCS'].sum()
+top_state = df_charging_station.loc[df_charging_station['No. of Operational PCS'].idxmax()]
+top_state_name = top_state['State']
+top_state_stations = top_state['No. of Operational PCS']
+
+# Display KPIs
+st.header("Public Charging Station Infrastructure of India")
+col10, col11, col12 = st.columns(3)
+
+with col10:
+    st.metric(label="State with most PCS", value=top_state_name)
+
+with col11:
+    st.metric(label=f"No. of PCS in {top_state_name}", value=top_state_stations)
+
+with col12:
+    st.metric(label="Total PCS in India", value=total_stations_india)
+
+charts.india_map_plot(df_charging_station,india_states)
+
+
+
+footer = """
+<style>
+a:link, a:visited {
+    color: red;
+    background-color: transparent;
+    text-decoration: solid;
+}
+
+a:hover, a:active {
+    color: blue;
+    background-color: transparent;
+    text-decoration: solid;
+}
+
+.footer {
+    position: relative; /* Make footer relative to the content */
+    bottom: 0;
+    width: 100%;
+    background-color: white;
+    color: black;
+    text-align: center;
+    margin-top: 20px; /* Adds spacing before the footer */
+    padding: 10px 0;  /* Adds padding inside the footer */
+    border-top: 1px solid #ddd; /* Optional: Add a subtle top border */
+}
+</style>
+<div class="footer">
+    <p>☀️Developed by Team Solar Circuit_031☀️ </p>
+    <p> 
+        <a href="https://www.linkedin.com/in/hemalatha-kasthuri-714919338" target="_blank">Hemalatha</a>, 
+        <a href="https://www.linkedin.com/in/dattasnehendu/" target="_blank">Snehendu</a>, 
+        <a href="https://www.linkedin.com/in/nikhil-kumar-001a812b0/" target="_blank">Nikhil</a>, & 
+        <a href="https://www.linkedin.com/in/ishita-agarwal-b06889268" target="_blank">Ishita</a>
+    </p>
+</div>
+"""
+st.markdown(footer, unsafe_allow_html=True)
